cfe10b39f9ff54a1fc44643c78ee1919e776b20c
Marcel Behrmann <info@eldoran.dev>
2020-12-06 09:46:59 +0100

    Adding chartjs

--- src/templates/member.vue
+++ src/templates/member.vue
@@ -1,6 +1,8 @@
 <template>
     <Layout>
-        <span>{{ $page }}</span>
+        {{ $page.member }}
+
+        <chart-line :data="starData" :options="{}" />
     </Layout>
 </template>
 
@@ -13,13 +15,46 @@ query ($memberId: ID){
             id
             timestamp
         }
+        medals {
+            id
+            day
+            part
+            place
+        }
     }
 }
 </page-query>
 
 <script>
-export default {
+import ChartLine from '../components/charts/line.vue';
 
+export default {
+    components: {
+        ChartLine,
+    },
+    computed: {
+        starData () {
+            const stars = [];
+            let count = 0;
+
+            stars.push({ x: 0, y: 0});
+            
+            for (let event of this.$page.member.events.sort((a, b) => a.timestamp - b.timestamp)) {
+                count++;
+                stars.push({
+                    x: event.timestamp,
+                    y: count,
+                });
+            }
+
+            return {
+                labels: [0, ...Array.from( (new Array(25)).keys()).map((k) => Date.UTC(2020, 11, k, 5, 0) / 1000)],
+                datasets: [ {
+                    data: stars
+                }],
+            };
+        }
+    }
 }
 </script>
 
